d895c5504787292369587d46f4d50fe0abdc0d30
podhmo <ababjam61@gmail.com>
2011-05-08 00:14:58 +0900

    exampleをつけた。

--- chibi-test.el
+++ chibi-test.el
@@ -20,6 +20,22 @@
 
 ;;; Commentary:
 
+;;; Example:
+;; (require 'chibi-test)
+;; (with-chibi-test*
+;;  (section "simplest example"
+;;           (test "1+1=2" 2 (+ 1 1))
+;;           (section "ng"
+;;                    (test "ng 1+1=2" 10 (+ 1 1)))
+
+;;           (defmacro let1 (b e &rest body)
+;;             `(let ((,b ,e)) ,@body))
+
+;;           (section "macro"
+;;                    (test: "macro test(let1)" 
+;;                           '(let ((b 10)) (* b b))
+;;                           (macro (let1 b 10 (* b b)))))))
+      
 ;; 
 
 ;;; Code:
